e50de3f7579acb6ccc7fda46509c9ff91fefe2e7
williberto14 <williberto14@gmail.com>
2020-12-16 01:09:23 -0500

    Le agregamos contenido y estilos al home del sitio

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,18 +1,49 @@
 <template>
-    <div>
-        <nav class="navbar navbar-light bg-light">
-            <form class="container-fluid justify-content-end">
-                <button class="btn btn-outline-success me-2" 
-                type="button"
-                @click="logOut"
-                >Cerrar Sesión</button>
+    <section>
+
+<!-- Barra de navegacion -->
+        <nav class="navbar navbar-light bg-dark justify-content-between">
+            <a class="navbar-brand text-light">Bienvenido</a>
+            <form class="form-inline">
+                <button class="btn btn-outline-light me-2" type="button" @click="logOut"> Cerrar Sesión</button>
             </form>
         </nav>
-        <div>
-            <p>La lógica {{user.nombre}}</p>
-            <p>{{user.email}}</p>
+
+        <div class="text-center mt-5">
+            <h1>¡BIENVENIDO A HOME!</h1>
+            <h4>Este sitio web está en construcción</h4>
         </div>
-    </div>
+
+                <!-- carrusel -->
+        <div class="container t-carrusel mt-3">
+            <div class=" justify-content-center">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://placeimg.com/500/250/nature" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://placeimg.com/500/250/people" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://placeimg.com/500/250/tech" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </div>
+            </div>
+        </div>
+            <div class="d-flex justify-content-center">
+                <img src="../assets/laTripleta.png" alt="" width="300" >
+            </div>
+    </section>
 </template>
 
 <script>
@@ -46,5 +77,13 @@ export default {
 </script>
 
 <style scoped>
-
+.t-carrusel{
+    width: 500px;
+    height: 250px;
+}
+.t-tripleta{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
 </style>
